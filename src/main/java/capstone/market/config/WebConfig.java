bc43f0d7eba34eb0cf854887408fa56a9fd6cc1c
MKSonny <minson9@naver.com>
2023-03-15 11:07:40 +0900

    testing again

--- src/main/java/capstone/market/config/WebConfig.java
+++ src/main/java/capstone/market/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 //    testing
+//    testing
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
