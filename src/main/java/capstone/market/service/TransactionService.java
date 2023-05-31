b69e480c43a0cec0fec3c57f41e78b981e81a169
dudtlr <wjsdudtlr86@naver.com>
2023-05-31 20:18:48 +0900

    구매내역 최신화

--- src/main/java/capstone/market/service/TransactionService.java
+++ src/main/java/capstone/market/service/TransactionService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -152,7 +153,7 @@ public class TransactionService {
 
     }
 
-    public List<PurchasedWithPostTitleDTO> getPurchasedListV2(){
+/*    public List<PurchasedWithPostTitleDTO> getPurchasedListV2(){
 
         List<Purchased> all = purchasedRepository.findAll();
 
@@ -160,5 +161,19 @@ public class TransactionService {
                 .collect(Collectors.toList());
 
         return getPurchasedList3;
+    }*/
+
+    public List<PurchasedWithPostTitleDTO> getPurchasedListV2(){
+
+        List<Purchased> all = purchasedRepository.findAll();
+
+        List<PurchasedWithPostTitleDTO> getPurchasedList3 = all.stream()
+                .sorted(Comparator.comparing(Purchased::getCreatedDate).reversed()) // 최신순으로 정렬
+                .map(u -> new PurchasedWithPostTitleDTO(u))
+                .collect(Collectors.toList());
+
+        return getPurchasedList3;
     }
+
+
 }
